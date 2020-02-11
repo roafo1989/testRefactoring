eff180bb73b23e425478a1691d7c17caf730ce22
roafo <roafo@mail.ru>
2020-02-11 20:10:21 +0300

    add lombook comments

--- src/main/java/data/Member.java
+++ src/main/java/data/Member.java
@@ -2,7 +2,8 @@ package data;
 
 import lombok.Data;
 
-@Data
+@Data//аннотация @Data библиотеки lombok создаёт конструкторы без аргументов, со всеми аргументами, а также геттеры и сеттеры
+     //помогает сокращать количество шаблонного кода
 public class Member {
  private final String name;
  private final Integer age;
--- src/main/java/data/MembersGroup.java
+++ src/main/java/data/MembersGroup.java
@@ -3,7 +3,9 @@ package data;
 import lombok.Data;
 
 import java.util.List;
-@Data
+
+@Data//аннотация @Data библиотеки lombok создаёт конструкторы без аргументов, со всеми аргументами, а также геттеры и сеттеры
+     //помогает сокращать количество шаблонного кода
 public class MembersGroup {
  private final String groupName;
  private final List<Member> members;
